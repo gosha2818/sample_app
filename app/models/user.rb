1e52945e7e4e8a29c55abf20c4ffef1e6006fdeb
Georgiy Nikiforov <gosha2818@gmail.com>
2013-12-23 10:16:55 -0600

    Sign in/out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_create :create_rememeber_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :name,  presence: true,
                     length: { maximum: 50 }
@@ -9,4 +10,18 @@ class User < ActiveRecord::Base
                     uniqueness: {case_sensitive: false}
   validates :password, length: { minimum: 8 }
   has_secure_password
+
+  def User.new_rememeber_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_rememeber_token
+    self.remember_token = User.encrypt(User.new_rememeber_token)
+  end
 end

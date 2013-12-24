a3c788a17d2169ec84c2e1269f35ebb290543185
Georgiy Nikiforov <gosha2818@gmail.com>
2013-12-24 11:46:19 -0600

    finish user edit, update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
 
+  before_action :signed_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+
+  def index
+      @users = User.paginate(page: params[:page], per_page: params[:count] ? params[:count] : 10)
   end
 
   def new
@@ -19,10 +36,42 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def current
+    @user = current_user
+    render 'show'
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    @user = User.find(params[:id])
+    unless current_user.admin? && !current_user?(@user)
+      redirect_to(root_url)
+    end
   end
 end
